730245cbc0d4b38e52dbf6e7a900e8ff3a413ab5
Emma Ogden <ecogden@gmail.com>
2018-09-28 10:28:26 +0100

    now returns true if card ids match

--- Api/factCards.js
+++ Api/factCards.js
@@ -16,58 +16,58 @@ const factCards = [
   {
     id: 194175810,
     imgFact: rabbitImg,
-    fact: 'Their last name means rabbit in portugese'
+    fact: 'Last name means rabbit in portugese'
   },
   { id: 197089210, imgFact: 'file-path', fact: 'They have pet gerbils' },
-  { id: 237214860, imgFact: germanyImg, fact: 'They are german' },
-  { id: 244095720, imgFact: almondImg, fact: 'Their name sounds like a nut' },
+  { id: 237214860, imgFact: germanyImg, fact: "They're german" },
+  { id: 244095720, imgFact: almondImg, fact: 'Name sounds like a nut' },
   {
     id: 265691610,
     imgFact: butterflyImg,
-    fact: "They're scared of butterflies"
+    fact: 'Scared of butterflies'
   },
   { id: 288389930, imgFact: guitarImg, fact: 'They were in a band' },
   {
     id: 298049330,
     imgFact: mosquitoImg,
-    fact: 'They were bitten by a radioactive mosquito'
+    fact: 'Bitten by a radioactive mosquito'
   },
   {
     id: 309636140,
     imgFact: bookImg,
-    fact: 'They were on the cover of a book'
+    fact: 'On the cover of a book'
   },
-  { id: 318501240, imgFact: biscuitImg, fact: 'They really love Jaffa Cakes' },
+  { id: 318501240, imgFact: biscuitImg, fact: 'Loves Jaffa Cakes' },
   {
     id: 318909150,
     imgFact: croatiaImg,
-    fact: 'They went on holiday to croatia and it was cold'
+    fact: 'Cold holiday in croatia'
   },
   {
     id: 321964960,
     imgFact: dogImg,
-    fact: "They want a dog (did not get one :'(')"
+    fact: 'Wants a dog'
   },
-  { id: 336875510, imgFact: 'file-path', fact: 'Artemis' },
+  { id: 336875510, imgFact: 'file-path', fact: 'From Athens' },
   {
     id: 352417160,
     imgFact: russiaImg,
-    fact: 'They got into trouble at the russian border'
+    fact: 'Got in trouble at the russian border'
   },
   {
     id: 371160610,
     imgFact: medalImg,
-    fact: 'They came 2nd in a taekwondo tournament'
+    fact: '2nd in a taekwondo tournament'
   },
   {
     id: 372279040,
     imgFact: speakImg,
-    fact: 'Can speak multiple languages (I think it was 4/5)'
+    fact: 'Speaks multiple languages'
   },
   {
     id: 375204010,
     imgFact: guineaPigImg,
-    fact: 'Has a chubby guinea pig (1kg i think she said)'
+    fact: 'Has a chubby guinea pig'
   }
 ];
 
